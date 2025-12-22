845afb6715621435a9a5d2d4340eb79029eed366
Rohitt <rohitingle126@gmail.com>
2025-12-22 15:15:21 +0530

    hs

---
+++ HS3.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+
+public class HS3 {
+
+	public static void main(String[] args) {
+		HashSet<String> hs=new HashSet<>();
+	int x=0;
+		hs.add("Red");
+		hs.add("White");
+		hs.add("Pink");
+		hs.add("Yellow");
+		hs.add("Black");
+		hs.add("Green");
+		System.out.println();
+		System.out.println(hs.size());
+		
+		for(int i=0;i<hs.size();i++)
+		{
+			x++;
+				
+		}
+		System.out.println(x);
+		
+		
+		
+		
+	}
+
+}
