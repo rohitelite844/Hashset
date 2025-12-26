57560c24ad745fed20a7c0fe8f09658befa760cd
Rohit Ingle <rohitingle126@gmail.com>
2025-12-26 12:38:42 +0530

    Add files via upload

---
+++ HS7.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+public class HS7 {
+
+	public static void main(String[] args) {
+		HashSet<String> hs=new HashSet<>();
+		hs.add("1");
+		hs.add("White");
+		hs.add("Pink");
+		hs.add("Yellow");
+		hs.add("Black");
+		hs.add("4");
+		hs.add("Green");
+		System.out.println(hs);
+		
+		System.out.println();
+
+	}
+
+}
